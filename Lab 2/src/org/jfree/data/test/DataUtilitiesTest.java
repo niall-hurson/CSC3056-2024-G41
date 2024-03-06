b11ee215486252379f109a43ff6d2a0debfdf73b
40298772 <nhurson01@qub.ac.uk>
2024-03-06 12:18:24 +0000

    updated data utils class

--- Lab 2/src/org/jfree/data/test/DataUtilitiesTest.java
+++ Lab 2/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -12,7 +12,11 @@ import junit.framework.TestCase;
 
 public class DataUtilitiesTest extends TestCase {
 	private Values2D values2D;
-    private KeyedValues keyedValues;
+    private KeyedValues keyedValuesAllPos;
+    private KeyedValues keyedValuesAllNeg;
+    private KeyedValues keyedValuesPosAndNeg;
+
+
 
 
 	@Before
@@ -22,11 +26,23 @@ public class DataUtilitiesTest extends TestCase {
 		testValues.addValue(1, 0, 0);
 		testValues.addValue(4, 1, 0);
 		
-		DefaultKeyedValues keyedValues = new DefaultKeyedValues();
-	    keyedValues.addValue("0", 5);
-	    keyedValues.addValue("1", 9);
-	    keyedValues.addValue("2", 2);
-	    this.keyedValues = keyedValues;
+		DefaultKeyedValues keyedValuesAllPos = new DefaultKeyedValues();
+		keyedValuesAllPos.addValue("0", 5);
+		keyedValuesAllPos.addValue("1", 9);
+		keyedValuesAllPos.addValue("2", 2);
+	    this.keyedValuesAllPos = keyedValuesAllPos;
+	    
+	    DefaultKeyedValues keyedValuesAllNeg = new DefaultKeyedValues();
+	    keyedValuesAllNeg.addValue("0", -5);
+	    keyedValuesAllNeg.addValue("1", -9);
+	    keyedValuesAllNeg.addValue("2", -2);
+	    this.keyedValuesAllNeg = keyedValuesAllNeg;
+	    
+	    DefaultKeyedValues keyedValuesPosAndNeg = new DefaultKeyedValues();
+	    keyedValuesPosAndNeg.addValue("0", -5);
+	    keyedValuesPosAndNeg.addValue("1", 9);
+	    keyedValuesPosAndNeg.addValue("2", -2);
+	    this.keyedValuesPosAndNeg = keyedValuesPosAndNeg;
 	}
 
 	public static void assertEquals(java.lang.String message, double expected, double actual, double delta) {
@@ -47,7 +63,7 @@ public class DataUtilitiesTest extends TestCase {
 	@Test
 	public void testInvalidColumnIndexColumnTotal() {
 		try {
-			DataUtilities.calculateColumnTotal(values2D, 1); // Column index exceeding the range.
+			DataUtilities.calculateColumnTotal(values2D, -1); // Column index exceeding the range.
 			fail("No exception thrown. The expected outcome was: a thrown "
 					+ "exception of type: IllegalArgumentException");
 		} catch (Exception e) {
@@ -69,14 +85,14 @@ public class DataUtilitiesTest extends TestCase {
 	// method 2 - calculateRowTotal
 	@Test
 	public void testValidDataAndRowTotal() {
-		assertEquals("Wrong sum returned. It should be 5.0", 5.0, DataUtilities.calculateRowTotal(values2D, 0),
+		assertEquals("Wrong sum returned. It should be 5.0", 5.0, DataUtilities.calculateRowTotal(values2D, 1),
 				0.0000001d);
 	}
 
 	@Test
 	public void testInvalidRowIndexRowTotal() {
 		try {
-			DataUtilities.calculateRowTotal(values2D, 1); // Row index exceeding the range.
+			DataUtilities.calculateRowTotal(values2D, 3); // Row index exceeding the range.
 			fail("No exception thrown. The expected outcome was: a thrown "
 					+ "exception of type: IllegalArgumentException");
 		} catch (Exception e) {
@@ -97,8 +113,32 @@ public class DataUtilitiesTest extends TestCase {
 	
 	//method 3 --create number array
     @Test
-    public void testValidDataCreateNumberArray() {
+    public void testValidDataAllPosCreateNumberArray() {
         double[] validData = {1.0, 2.5, 3.7};
+        Number[] result = DataUtilities.createNumberArray(validData);
+        assertNotNull("Result should not be null", result);
+        assertEquals("Array length should match input length", validData.length, result.length);
+        for (int i = 0; i < validData.length; i++) {
+            assertNotNull("Element should not be null", result[i]); // Add null check
+            assertEquals("Element value should match", validData[i], result[i].doubleValue(), 0.000001d);
+        }
+    }
+    
+    @Test
+    public void testValidDataPosAndNegCreateNumberArray() {
+        double[] validData = {-2, 0, 5};
+        Number[] result = DataUtilities.createNumberArray(validData);
+        assertNotNull("Result should not be null", result);
+        assertEquals("Array length should match input length", validData.length, result.length);
+        for (int i = 0; i < validData.length; i++) {
+            assertNotNull("Element should not be null", result[i]); // Add null check
+            assertEquals("Element value should match", validData[i], result[i].doubleValue(), 0.000001d);
+        }
+    }
+    
+    @Test
+    public void testValidDataAllNegCreateNumberArray() {
+        double[] validData = {-3, -6, 12};
         Number[] result = DataUtilities.createNumberArray(validData);
         assertNotNull("Result should not be null", result);
         assertEquals("Array length should match input length", validData.length, result.length);
@@ -124,8 +164,40 @@ public class DataUtilitiesTest extends TestCase {
     //method 4
  // Test for valid non-null array of double arrays
     @Test
-    public void testValidDataCreateNumberArray2D() {
+    public void testValidDataAllPosCreateNumberArray2D() {
         double[][] validData = {{1.0, 2.5}, {3.7, 4.2}};
+        Number[][] result = DataUtilities.createNumberArray2D(validData);
+        assertNotNull("Result should not be null", result);
+        assertEquals("Number of rows should match", validData.length, result.length);
+        for (int i = 0; i < validData.length; i++) {
+            assertNotNull("Row " + i + " should not be null", result[i]); // Add null check for row
+            assertEquals("Number of columns in row " + i + " should match", validData[i].length, result[i].length);
+            for (int j = 0; j < validData[i].length; j++) {
+                assertNotNull("Element [" + i + "][" + j + "] should not be null", result[i][j]); // Add null check for element
+                assertEquals("Element value should match", validData[i][j], result[i][j].doubleValue(), 0.000001d);
+            }
+        }
+    }
+    
+    @Test
+    public void testValidDataAllNegCreateNumberArray2D() {
+        double[][] validData = {{-1.0, -2.5}, {-3.7, -4.2}};
+        Number[][] result = DataUtilities.createNumberArray2D(validData);
+        assertNotNull("Result should not be null", result);
+        assertEquals("Number of rows should match", validData.length, result.length);
+        for (int i = 0; i < validData.length; i++) {
+            assertNotNull("Row " + i + " should not be null", result[i]); // Add null check for row
+            assertEquals("Number of columns in row " + i + " should match", validData[i].length, result[i].length);
+            for (int j = 0; j < validData[i].length; j++) {
+                assertNotNull("Element [" + i + "][" + j + "] should not be null", result[i][j]); // Add null check for element
+                assertEquals("Element value should match", validData[i][j], result[i][j].doubleValue(), 0.000001d);
+            }
+        }
+    }
+    
+    @Test
+    public void testValidDataPosAndNegCreateNumberArray2D() {
+        double[][] validData = {{1.0, -2.5}, {-3.7, 4.2}};
         Number[][] result = DataUtilities.createNumberArray2D(validData);
         assertNotNull("Result should not be null", result);
         assertEquals("Number of rows should match", validData.length, result.length);
@@ -154,14 +226,42 @@ public class DataUtilitiesTest extends TestCase {
     
     //method 5 
     @Test
-    public void testValidDataGetCumulativePercentages() {
-        KeyedValues result = DataUtilities.getCumulativePercentages(keyedValues);
+    public void testValidDataAllPosGetCumulativePercentages() {
+        KeyedValues result = DataUtilities.getCumulativePercentages(keyedValuesAllPos);
         assertNotNull("Result should not be null", result);
-        assertEquals("Number of items should match", keyedValues.getItemCount(), result.getItemCount());
+        assertEquals("Number of items should match", keyedValuesAllPos.getItemCount(), result.getItemCount());
         double totalValue = 16; // Sum of all values (5 + 9 + 2)
         double cumulativeSum = 0;
-        for (int i = 0; i < keyedValues.getItemCount(); i++) {
-            cumulativeSum += keyedValues.getValue(i).doubleValue();
+        for (int i = 0; i < keyedValuesAllPos.getItemCount(); i++) {
+            cumulativeSum += keyedValuesAllPos.getValue(i).doubleValue();
+            double expectedPercentage = cumulativeSum / totalValue;
+            assertEquals("Cumulative percentage should match", expectedPercentage, result.getValue(i).doubleValue(), 0.000001d);
+        }
+    }
+    
+    @Test
+    public void testValidDataAllNegGetCumulativePercentages() {
+        KeyedValues result = DataUtilities.getCumulativePercentages(keyedValuesAllNeg);
+        assertNotNull("Result should not be null", result);
+        assertEquals("Number of items should match", keyedValuesAllNeg.getItemCount(), result.getItemCount());
+        double totalValue = 16; // Sum of all values (5 + 9 + 2)
+        double cumulativeSum = 0;
+        for (int i = 0; i < keyedValuesAllNeg.getItemCount(); i++) {
+            cumulativeSum += keyedValuesAllNeg.getValue(i).doubleValue();
+            double expectedPercentage = cumulativeSum / totalValue;
+            assertEquals("Cumulative percentage should match", expectedPercentage, result.getValue(i).doubleValue(), 0.000001d);
+        }
+    }
+    
+    @Test
+    public void testValidDataPosAndNegGetCumulativePercentages() {
+        KeyedValues result = DataUtilities.getCumulativePercentages(keyedValuesPosAndNeg);
+        assertNotNull("Result should not be null", result);
+        assertEquals("Number of items should match", keyedValuesPosAndNeg.getItemCount(), result.getItemCount());
+        double totalValue = 16; // Sum of all values (5 + 9 + 2)
+        double cumulativeSum = 0;
+        for (int i = 0; i < keyedValuesPosAndNeg.getItemCount(); i++) {
+            cumulativeSum += keyedValuesPosAndNeg.getValue(i).doubleValue();
             double expectedPercentage = cumulativeSum / totalValue;
             assertEquals("Cumulative percentage should match", expectedPercentage, result.getValue(i).doubleValue(), 0.000001d);
         }
